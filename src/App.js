608afcc0aa8897f985e9faa3b8a757dba054395c
Dhurv2608 <dhruvkatrodiya34@gmail.com>
2023-03-20 10:40:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Routes , Route} from 'react-router-dom';
+import Home from './Home';
+
+const App = () => {
+
+
+  return (
+    <div>
+    <Home />
+      
+    </div>
+  )
+}
+
+export default App
